f19d35be8f0dd0fe6879b0e471e537743a07ce95
anthonyjclark <anthonyjclark@gmail.com>
2024-07-15 11:58:50 -0700

    Added simple setup.py file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reviewlogger",
+    version="1.0",
+    description="A logging library for the [Review](https://review.github.io/) visualizer.",
+    author="Anthony J. Clark",
+    license="MIT",
+    packages=["reviewlogger"],
+)
